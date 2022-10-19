32dcae7b5bc1cb22d4ffb11fbe7be2ee6ac10872
Nisha <nisha.chavan@appliedtechnology.se>
2022-10-19 21:58:14 +0200

    Set Up Complete - Oct 19 - 2022

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/main.scss';
+// Create heading node
+const heading = document.createElement('h1');
+heading.textContent = 'Interesting!';
+
+// Append heading node to the DOM
+const app = document.querySelector('#root');
+app.append(heading);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+   // contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    }),
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+}
